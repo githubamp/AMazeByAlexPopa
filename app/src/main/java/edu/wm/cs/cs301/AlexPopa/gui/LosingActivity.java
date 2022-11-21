a2d83481a0f7818985d7df01b133fb49510724f7
Alex Popa <ampopa@wm.edu>
2022-11-20 20:45:47 -0500

    Finished LosingActivity

--- app/src/main/java/edu/wm/cs/cs301/AlexPopa/gui/LosingActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/AlexPopa/gui/LosingActivity.java
@@ -1,6 +1,9 @@
 package edu.wm.cs.cs301.AlexPopa.gui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +15,18 @@ public class LosingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lose);
+        Intent information = getIntent();
+
+        int pathLength = information.getIntExtra("Steps taken", 0);
+        int small = information.getIntExtra("Shortest steps", 0);
+        int spent = information.getIntExtra("Energy", 0);
+
+        EditText steps = (EditText) findViewById(R.id.steps2);
+        EditText shortest = (EditText) findViewById(R.id.shortest2);
+        EditText energy = (EditText) findViewById(R.id.energyspent2);
+
+        steps.setText("You traveled " + pathLength + " steps");
+        shortest.setText("The shortest path length was " + small + " steps");
+        energy.setText("Energy spent: " + spent);
     }
 }
